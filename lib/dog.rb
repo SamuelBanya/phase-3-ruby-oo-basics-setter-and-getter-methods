d51ae567f25195c6d5c0ce710eeec897c3fbd971
Samuel Banya <sbanya@fastmail.com>
2022-08-22 19:10:45 -0400

    commit tree cbbc6882c70a08b8d5a91a757ccec05f85f14105

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,21 @@
 class Dog
+  # Setter:
+  def name=(name)
+    @name = name
+  end
 
+  # Getter:
+  def name
+    @name
+  end
+
+  # Setter:
+  def breed=(breed)
+    @breed = breed
+  end
+
+  # Getter:
+  def breed
+    @breed
+  end
 end
--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,31 @@
 class Person
+  # Setter method:
+  # NOTE: The 'Setter' method always has the '=' in its name:
+  def name=(name)
+    @name = name
+  end
 
+  # Getter method:
+  def name
+    @name
+  end
+
+  def job=(job)
+    @job = job
+  end
+
+  def job
+    @job
+  end
 end
+
+kanye = Person.new()
+# One way of calling the 'Setter' method:
+# kanye.name=("Kanye")
+
+# Easier shorthand version of using the 'Setter' method:
+kanye.name = "Kanye2"
+kanye.job = "Influencer"
+
+puts "kanye.name: ", kanye.name
+puts "kanye.job: ", kanye.job
